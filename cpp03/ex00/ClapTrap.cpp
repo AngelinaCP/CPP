d93c069cd79414ac8a46e95e0a623e50c49d781e
Dwemer Delena <ddelena@er-a1.kzn.21-school.ru>
2022-03-16 15:37:10 +0300

    added cpp03

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -1,3 +1,23 @@
 #include "ClapTrap.hpp"
 
-void ClapTrap::
+ClapTrap::ClapTrap(std::string name) {
+    ClapTrap::name = name;
+}
+ClapTrap::~ClapTrap() {}
+
+void    ClapTrap::attack(const std::string& target) {
+
+    std::cout   << "ClapTrap " << this->name << " attacks "
+                << target << ", causing " << attack_damage
+                << " points of damage!" << std::endl;
+    this->energy_points--;
+}
+
+void    ClapTrap::takeDamage(unsigned int amount) {
+
+}
+
+void    ClapTrap::beRepaired(unsigned int amount) {
+    this->hit_points += amount;
+    this->energy_points--;
+}
